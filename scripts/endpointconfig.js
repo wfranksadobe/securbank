e279f1155fad887566f3a0f10a3955b0f59be901
William Franks <136557841+wfranksadobe@users.noreply.github.com>
2025-03-18 09:50:12 +1100

    Update endpointconfig.js

--- scripts/endpointconfig.js
+++ scripts/endpointconfig.js
@@ -1,9 +1,9 @@
 function getAEMPublish() {
-  return 'https://publish-p137825-e1511252.adobeaemcloud.com';
+  return 'https://publish-p137176-e1382526.adobeaemcloud.com';
 }
 
 function getAEMAuthor() {
-  return 'https://author-p137825-e1511252.adobeaemcloud.com';
+  return 'https://author-p137176-e1382526.adobeaemcloud.com';
 }
 
 export { getAEMPublish, getAEMAuthor };
